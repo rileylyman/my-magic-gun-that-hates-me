78376a4ee0d17ef54b65ce289be101aff4ec6df2
Hansong Li <hl6349@nyu.edu>
2026-07-24 17:49:09 -0400

    expose rarity field for artifacts

--- src/artifact.gd
+++ src/artifact.gd
@@ -1,8 +1,16 @@
 class_name Artifact
 extends Node2D
 
+enum ArtifactRarity {
+	COMMON,
+	UNCOMMON,
+	RARE,
+	LEGENDARY
+}
+
 @export var title: String
 @export_multiline var description: String
+@export var rarity: ArtifactRarity
 
 func pre_tick_callback(_state: TickState) -> void:
 	pass
