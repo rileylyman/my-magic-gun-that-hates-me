6ae10653e114a955ef5e4b0561b39de4da9bc06a
humanpersonssb <90877716+humanpersonssb@users.noreply.github.com>
2026-07-25 18:32:02 -0400

    dartboard
    dart thrown sound effect

---
+++ resources/artifacts/dartboard.gd
@@ -0,0 +1,23 @@
+extends Artifact
+var target = randi_range(lowtar, hightar)
+var hightar = 15
+var lowtar = 5
+var hits = 0
+var hashit = false
+func post_tick_callback(state: TickState) -> void:
+	for c in state.cards:
+		if c.curr <= 0:
+			hits +=1
+	if  hits >= target and !hashit:
+			state.score *= target
+			await shake("x" + target)
+			hashit = true
+	
+	if state.days == 1:
+		target = randi_range(lowtar, hightar)
+		await shake()
+		await shake(" Tagret: " + target + " ")
+
+func hand_submit_callback(_state: TickState) -> void:
+	hits = 0
+	hashit = false
