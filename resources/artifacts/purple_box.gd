f7320949afa6c75a03768290eb779806206c8e3f
humanpersonssb <90877716+humanpersonssb@users.noreply.github.com>
2026-07-25 15:02:40 -0400

    purplebox

--- resources/artifacts/purple_box.gd
+++ resources/artifacts/purple_box.gd
@@ -2,8 +2,8 @@ extends Artifact
 
 func post_tick_callback(state: TickState) -> void:
 	for c in state.cards:
-		if c.max_value < 0:
-			return
+		if c.curr > 0:
+			continue
 		var root: int = int(sqrt(c.max_value))
 		if root * root == c.max_value:
 			if(state.score>0):
