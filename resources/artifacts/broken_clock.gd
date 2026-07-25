aac05c5cb69a008c01e766e61e850c74e5daee19
humanpersonssb <90877716+humanpersonssb@users.noreply.github.com>
2026-07-24 21:12:19 -0400

    Relics
    Rellics

---
+++ resources/artifacts/broken_clock.gd
@@ -0,0 +1,14 @@
+extends Artifact
+
+func pre_tick_callback(state: TickState) -> void:
+	for c in state.cards:
+		if c.max_value % 2 == 1:
+			if state.days % 2 == 1:
+				c.curr -= 1
+			if state.days % 2 == 0:
+				c.curr += 1
+		if c.max_value % 2 == 0:
+			if state.days % 2 == 0:
+				c.curr -= 1
+			if state.days % 2 == 1:
+				c.curr += 1
---
+++ resources/artifacts/golden_calendar.gd
@@ -0,0 +1,16 @@
+extends Artifact
+var i = 0
+var cd = 0
+func post_tick_callback(state: TickState) -> void:
+	i = 0
+	for c in state.cards:
+		if c.curr <= 0:
+			i = i +1
+	if i == 1:
+		cd += 1
+	if cd == 2:
+		state.gm.active_counter.value +=1
+		cd = 0
+
+func hand_submit_callback(_state: TickState) -> void:
+	cd = 0
