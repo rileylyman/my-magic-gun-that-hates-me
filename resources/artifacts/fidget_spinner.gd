4c339d488acfdf488c53629298a10e49ae812f43
Riley Lyman <rileylyman@gmail.com>
2026-07-25 17:09:38 -0400

    new fidget thing

--- resources/artifacts/fidget_spinner.gd
+++ resources/artifacts/fidget_spinner.gd
@@ -1,6 +1,19 @@
 extends Artifact
 
 func post_tick_callback(state: TickState) -> void:
+	var hit = 1
+	for i in range(state.cards.size()):
+		for j in range(state.cards.size()):
+			for k in range(state.cards.size()):
+				if i == j or i == k or j == k:
+					continue
+				if state.cards[i].max_value == state.cards[j].max_value and state.cards[i].max_value == state.cards[k].max_value and state.cards[i].curr <= 0 and state.cards[j].curr <= 0 and state.cards[k].curr <= 0:
+					hit = max(hit, state.cards[i].max_value)
+	if hit > 1:
+		state.score *= hit * 3
+		await shake("x"+str(hit))
+
+func _post_tick_callback(state: TickState) -> void:
 	var x = 0
 	var y = 0
 	var z = 0
