3f797407c8063438ed7f66e33d4c663813f563b6
humanpersonssb <90877716+humanpersonssb@users.noreply.github.com>
2026-07-25 14:21:50 -0400

    roupe

---
+++ resources/artifacts/toupee.gd
@@ -0,0 +1,10 @@
+extends Artifact
+var curses = 0
+func post_tick_callback(state: TickState) -> void:
+	state.score *= curses
+
+func encounter_start_callback() -> void:
+	curses = 0
+	for a in GlobalManager.artifacts:
+		if a.rarity == ArtifactRarity.CURSED:
+			curses +=1
