500443d91d99657caed20ea9ab4ceecf9e8c30d2
Riley Lyman <rileylyman@gmail.com>
2026-07-25 17:10:04 -0400

    hey

--- resources/artifacts/fidget_spinner.gd
+++ resources/artifacts/fidget_spinner.gd
@@ -12,21 +12,3 @@ func post_tick_callback(state: TickState) -> void:
 	if hit > 1:
 		state.score *= hit * 3
 		await shake("x"+str(hit))
-
-func _post_tick_callback(state: TickState) -> void:
-	var x = 0
-	var y = 0
-	var z = 0
-	var hit = 1
-	for c in state.cards:
-		for i in state.cards:
-			for r in state.cards:
-				if c.max_value == i.max_value and c.max_value == r.max_value and c.curr <= 0 and r.curr <= 0 and i.curr <= 0 and x != y and x!=z and y!= z:
-					hit = c.max_value
-				z+=1
-			y+=1
-		x+=1
-	if hit > 1:
-		hit *=3
-		state.score *= hit
-		await shake("x"+str(hit))
