de411c367ff363580fef19c0b042de485122dd0c
Riley Lyman <rileylyman@gmail.com>
2026-07-26 00:26:11 -0400

    add view full deck to main screen

--- src/deck_view.gd
+++ src/deck_view.gd
@@ -1,10 +1,30 @@
 extends Control
 
+signal closed
+
+@export var use_duplicate_cards: bool = false
+
+const card_scene: PackedScene = preload("res://src/card.tscn")
+
 @onready var grid_container: GridContainer = %GridContainer
+
+var _original_parents: Dictionary = {}
 
 
 func _ready() -> void:
+	%SkipArtifactsButton.pressed.connect(close)
 	setup_cards()
+
+
+func open() -> void:
+	setup_cards()
+	visible = true
+
+
+func close() -> void:
+	restore_cards()
+	visible = false
+	closed.emit()
 
 
 func setup_cards() -> void:
@@ -13,22 +33,68 @@ func setup_cards() -> void:
 			grid_container.remove_child(child)
 			child.queue_free()
 
-	for card in GlobalManager.deck:
-		if card.get_parent() != null:
-			card.get_parent().remove_child(card)
+	_original_parents.clear()
 
-		card.prepare_for_task_grid()
-		grid_container.add_child(card)
-		card.do_setup()
+	for card in GlobalManager.deck:
+		var display_card: Card = card
+
+		if use_duplicate_cards:
+			display_card = make_duplicate_card(card)
+		else:
+			_original_parents[card] = card.get_parent()
+
+			if card.get_parent() != null:
+				card.get_parent().remove_child(card)
+
+		display_card.apply_scene_size()
+		display_card.rotation = 0
+		display_card.scale = Vector2.ONE
+		display_card.position = Vector2.ZERO
+		display_card.visible = true
+
+		grid_container.add_child(display_card)
+
+		if use_duplicate_cards:
+			display_card.do_setup()
+
+
+func make_duplicate_card(card: Card) -> Card:
+	var display_card := card_scene.instantiate() as Card
+
+	display_card.max_value = card.max_value
+	display_card.curr = card.curr
+	display_card.show_damage = card.show_damage
+	display_card.show_zero_on_damage = card.show_zero_on_damage
+
+	if card.has_stamp:
+		display_card.set_stamp(card.stamp.duplicate())
+
+	return display_card
+
+
+func restore_cards() -> void:
+	if use_duplicate_cards:
+		for child in grid_container.get_children():
+			if child is Card:
+				grid_container.remove_child(child)
+				child.queue_free()
+
+		return
+
+	for card in _original_parents:
+		if not is_instance_valid(card):
+			continue
+
+		if card.get_parent() == grid_container:
+			grid_container.remove_child(card)
+
+		var original_parent: Node = _original_parents[card]
+
+		if original_parent != null and is_instance_valid(original_parent):
+			original_parent.add_child(card)
+
+	_original_parents.clear()
 
 
 func _exit_tree() -> void:
-	if not is_instance_valid(grid_container):
-		return
-
-	for child in grid_container.get_children():
-		if child is not Card:
-			continue
-
-		grid_container.remove_child(child)
-		child.prepare_for_battle()
+	restore_cards()
--- src/encounter_screen.gd
+++ src/encounter_screen.gd
@@ -1,6 +1,24 @@
 extends Control
 
 
+func _ready() -> void:
+	%ViewFullDeckButton.mouse_entered.connect(_on_view_full_deck_mouse_entered)
+	%ViewFullDeckButton.mouse_exited.connect(_on_view_full_deck_mouse_exited)
+	%ViewFullDeckButton.pressed.connect(_on_view_full_deck_pressed)
+
+
+func _on_view_full_deck_mouse_entered() -> void:
+	%ViewFullDeckButton.get_node("Tooltip").visible = true
+
+
+func _on_view_full_deck_mouse_exited() -> void:
+	%ViewFullDeckButton.get_node("Tooltip").visible = false
+
+
+func _on_view_full_deck_pressed() -> void:
+	%Deck.open()
+
+
 func _process(_delta: float) -> void:
 	# print(get_viewport().gui_get_hovered_control())
 	pass
