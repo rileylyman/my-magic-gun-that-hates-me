e1619f069d1bbc182be01989e5d8008472c4823f
davidtheli <soulfulpumpkin@gmail.com>
2026-07-26 12:01:22 -0400

    printer

--- resources/artifacts/photocopier.gd
+++ resources/artifacts/photocopier.gd
@@ -5,4 +5,5 @@ func hand_submit_callback(state: TickState) -> void:
 	if state.cards.size() == 1:
 		await shake(" Copied! ")
 		var card = state.cards[0].duplicate()
+		card.stamp = state.cards[0].stamp.duplicate()
 		state.gm.add_to_hand(card)
